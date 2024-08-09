fa58898236a4233b248cfb42c778d8c36ef2cbb5
Brinaldy Alexis <brinaldyalexis@hotmail.com>
2024-08-09 18:55:14 -0400

    chore: Refactor app.js to use socket.io for real-time updates

--- app.js
+++ app.js
@@ -5,31 +5,31 @@ const socketIO = require('socket.io');
 const cron = require('node-cron');
 
 const app = express();
-// const server = http.createServer(app);
-// const io = socketIO(server);
+const server = http.createServer(app);
+const io = socketIO(server);
 
-// const publicPath = path.resolve(__dirname, 'public');
-// app.use(express.static(publicPath));
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
 
-// // Configurar eventos de socket.io
-// io.on('connection', client => {
-//     console.log('Cliente conectado');
+// Configurar eventos de socket.io
+io.on('connection', client => {
+    console.log('Cliente conectado');
 
-//     client.on('disconnect', () => {
-//        console.log('Cliente desconectado');
-//     });
+    client.on('disconnect', () => {
+       console.log('Cliente desconectado');
+    });
 
-//     client.on('livescores', (data) => {
-//       // Actualizar los datos en el cliente
-//       console.log(data);
-//     });
-// });
+    client.on('livescores', (data) => {
+      // Actualizar los datos en el cliente
+      console.log(data);
+    });
+});
 
-// // Configurar tarea cron para emitir "hola mundo" en el evento livescores cada 5 segundos
-// cron.schedule('*/5 * * * * *', () => {
-//     io.emit('livescores', 'hola mundo');
-// });
+// Configurar tarea cron para emitir "hola mundo" en el evento livescores cada 5 segundos
+cron.schedule('*/5 * * * * *', () => {
+    io.emit('livescores', 'hola mundo');
+});
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
